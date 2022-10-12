3078d765ae4bfcbab2b2c521f1efb6cfd8b306d6
IffatNur <iffatnoorshad00@gmail.com>
2022-10-12 13:26:38 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from "react";
 import { useLoaderData } from 'react-router-dom';
 import {
   LineChart,
@@ -16,39 +16,27 @@ const Statistics = () => {
     const topics = useLoaderData().data;
     console.log(topics);
     return (
-        <div>
-            {
-                topics.map(topic=> {
-                    <div>
-                      <ResponsiveContainer width="100%" height="100%">
-                        <LineChart
-                          width={500}
-                          height={300}
-                          data={topic}
-                          margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                          }}
-                        >
-                          <CartesianGrid strokeDasharray="3 3" />
-                          <XAxis dataKey={topic.name} />
-                          <YAxis />
-                          <Tooltip />
-                          <Legend />
-                          <Line
-                            type="monotone"
-                            dataKey={topic.total}
-                            stroke="#8884d8"
-                            activeDot={{ r: 8 }}
-                          />
-                        </LineChart>
-                      </ResponsiveContainer>
-                    </div>;
-                })
-            }
-        </div>
+      <div className="w-2/4 md:w-full">
+        <LineChart
+          className="mx-auto"
+          width={700}
+          height={500}
+          data={topics}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </div>
     );
 };
 
